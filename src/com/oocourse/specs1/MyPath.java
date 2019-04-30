debcd4142683b7e62b2c71a664f28ef7873c0f86
zwx <853845818@qq.com>
2019-04-30 13:59:13 +0800

    checkstyle

--- src/com/oocourse/specs1/MyPath.java
+++ src/com/oocourse/specs1/MyPath.java
@@ -2,33 +2,27 @@ package com.oocourse.specs1;
 
 import com.oocourse.specs1.models.Path;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.TreeSet;
 
 public class MyPath implements Path {
 
     private ArrayList<Integer> list;
-    private TreeSet<Integer> s;
+    private TreeSet<Integer> nodeSet;
 
     public MyPath(int[] nodeList) {
         list = new ArrayList<>();
         for (int i : nodeList) {
             list.add(i);
         }
-        s = new TreeSet<>(list);
-        //list = new ArrayList<Integer>(Arrays.asList(nodeList));
+        nodeSet = new TreeSet<>(list);
     }
-    // Iterable<Integer>和Comparable<Path>接口的规格请参阅JDK
-    //@ public instance model non_null int[] nodes;
 
-    //@ ensures \result == nodes.length;
     public /*@pure@*/int size() {
         return list.size();
     }
 
-    /*@ requires index >= 0 && index < size();
-      @ assignable \nothing;
-      @ ensures \result == nodes[index];
-      @*/
     public /*@pure@*/ int getNode(int index) {
         if (index < 0 || index >= list.size()) {
             throw new IndexOutOfBoundsException();
@@ -36,30 +30,14 @@ public class MyPath implements Path {
         return list.get(index);
     }
 
-    //@ ensures \result == (\exists int i; 0 <= i && i < nodes.length; nodes[i] == node);
     public /*@pure@*/ boolean containsNode(int node) {
-        return s.contains(node);
+        return nodeSet.contains(node);
     }
 
-    /*@ ensures \result == (\num_of int i, j; 0 <= i && i < j && j < nodes.length;
-                             nodes[i] != nodes[j]);
-      @*/
     public /*pure*/ int getDistinctNodeCount() {
-        return s.size();
+        return nodeSet.size();
     }
 
-    /*@ also
-      @ public normal_behavior
-      @ requires obj != null && obj instanceof Path;
-      @ assignable \nothing;
-      @ ensures \result == ((Path) obj).nodes.length == nodes.length) &&
-      @                      (\forall int i; 0 <= i && i < nodes.length; nodes[i] == ((Path) obj).nodes[i]);
-      @ also
-      @ public normal_behavior
-      @ requires obj == null || !(obj instanceof Path);
-      @ assignable \nothing;
-      @ ensures \result == false;
-      @*/
     public boolean equals(Object obj) {
         if (!(obj instanceof Path)) {
             return false;
@@ -76,7 +54,6 @@ public class MyPath implements Path {
         return true;
     }
 
-    //@ ensures \result == (nodes.length >= 2);
     public /*@pure@*/ boolean isValid() {
         return list.size() >= 2;
     }
--- src/com/oocourse/specs1/MyPathContainer.java
+++ src/com/oocourse/specs1/MyPathContainer.java
@@ -5,7 +5,7 @@ import com.oocourse.specs1.models.PathContainer;
 import com.oocourse.specs1.models.PathIdNotFoundException;
 import com.oocourse.specs1.models.PathNotFoundException;
 
-import java.util.*;
+import java.util.TreeMap;
 
 public class MyPathContainer implements PathContainer {
 
@@ -19,19 +19,11 @@ public class MyPathContainer implements PathContainer {
         pidList = new Path[3005];
         nodes = new TreeMap<>();
     }
-    //@ public instance model non_null Path[] pList;
-    //@ public instance model non_null int[] pidList;
 
-    //@ ensures \result == pList.length;
     public /*@pure@*/int size() {
         return plist.size();
     }
 
-    /*@ requires path != null;
-      @ assignable \nothing;
-      @ ensures \result == (\exists int i; 0 <= i && i < pList.length;
-      @                     pList[i].equals(path));
-      @*/
     public /*@pure@*/ boolean containsPath(Path path) {
         if (path == null) {
             throw new NullPointerException();
@@ -39,9 +31,6 @@ public class MyPathContainer implements PathContainer {
         return plist.containsKey(path);
     }
 
-    /*@ ensures \result == (\exists int i; 0 <= i && i < pidList.length;
-      @                      pidList[i] == pathId);
-      @*/
     public /*@pure@*/ boolean containsPathId(int pathId) {
         if (pathId <= 0 || pathId > 3000) {
             return false;
@@ -49,33 +38,14 @@ public class MyPathContainer implements PathContainer {
         return pidList[pathId] != null;
     }
 
-    /*@ public normal_behavior
-      @ requires containsPathId(pathId);
-      @ assignable \nothing;
-      @ ensures (pidList.length == pList.length)&&(\exists int i; 0 <= i && i < pList.length; pidList[i] == pathId && \result == pList[i]);
-      @ also
-      @ public exceptional_behavior
-      @ requires !containsPathId(pathId);
-      @ assignable \nothing;
-      @ signals_only PathIdNotFoundException;
-      @*/
-    public /*@pure@*/ Path getPathById(int pathId) throws PathIdNotFoundException {
+    public /*@pure@*/ Path getPathById(int pathId)
+            throws PathIdNotFoundException {
         if (!containsPathId(pathId)) {
             throw new PathIdNotFoundException(pathId);
         }
         return pidList[pathId];
     }
 
-    /*@ public normal_behavior
-      @ requires path != null && path.isValid() && containsPath(path);
-      @ assignable \nothing;
-      @ ensures (pidList.length == pList.length) && (\exists int i; 0 <= i && i < pList.length; pList[i].equals(path) && pidList[i] == \result);
-      @ also
-      @ public exceptional_behavior
-      @ signals (PathNotFoundException e) path == null;
-      @ signals (PathNotFoundException e) !path.isValid();
-      @ signals (PathNotFoundException e) !containsPath(path);
-      @*/
     public /*@pure@*/ int getPathId(Path path) throws PathNotFoundException {
         if (path == null || !path.isValid() || !containsPath(path)) {
             throw new PathNotFoundException(path);
@@ -83,23 +53,6 @@ public class MyPathContainer implements PathContainer {
         return plist.get(path);
     }
 
-    /*@ normal_behavior
-      @ requires path != null && path.isValid();
-      @ assignable pList, pidList;
-      @ ensures (pidList.length == pList.length);
-      @ ensures (\exists int i; 0 <= i && i < pList.length; pList[i] == path &&
-      @           \result == pidList[i]);
-      @ ensures !\old(containsPath(path)) ==>
-      @          pList.length == (\old(pList.length) + 1) &&
-      @          pidList.length == (\old(pidList.length) + 1);
-      @ ensures (\forall int i; 0 <= i && i < \old(pList.length);
-      @          containsPath(\old(pList[i])) && containsPathId(\old(pidList[i])));
-      @ also
-      @ normal_behavior
-      @ requires path == null || path.isValid() == false;
-      @ assignable \nothing;
-      @ ensures \result == 0;
-      @*/
     public int addPath(Path path) {
         if (path == null || !path.isValid()) {
             return 0;
@@ -111,30 +64,15 @@ public class MyPathContainer implements PathContainer {
             pidList[id] = path;
             for (int i : path) {
                 if (nodes.containsKey(i)) {
-                    nodes.put(i,nodes.get(i)+1);
-                }
-                else {
-                    nodes.put(i,1);
+                    nodes.put(i, nodes.get(i) + 1);
+                } else {
+                    nodes.put(i, 1);
                 }
             }
             return id++;
         }
     }
 
-    /*@ public normal_behavior
-      @ requires path != null && path.isValid() && \old(containsPath(path));
-      @ assignable pList, pidList;
-      @ ensures containsPath(path) == false;
-      @ ensures (pidList.length == pList.length);
-      @ ensures (\exists int i; 0 <= i && i < \old(pList.length); \old(pList[i].equals(path)) &&
-      @           \result == \old(pidList[i]));
-      @ also
-      @ public exceptional_behavior
-      @ assignable \nothing;
-      @ signals (PathNotFoundException e) path == null;
-      @ signals (PathNotFoundException e) path.isValid()==false;
-      @ signals (PathNotFoundException e) !containsPath(path);
-      @*/
     public int removePath(Path path) throws PathNotFoundException {
         if (path == null || !path.isValid() || !containsPath(path)) {
             throw new PathNotFoundException(path);
@@ -144,7 +82,7 @@ public class MyPathContainer implements PathContainer {
         pidList[ret] = null;
         for (int i : path) {
             int tmp = nodes.get(i);
-            nodes.put(i,tmp-1);
+            nodes.put(i, tmp - 1);
             if (tmp == 1) {
                 nodes.remove(i);
             }
@@ -152,17 +90,6 @@ public class MyPathContainer implements PathContainer {
         return ret;
     }
 
-    /*@ public normal_behavior
-      @ requires \old(containsPathId(pathId));
-      @ assignable pList, pidList;
-      @ ensures pList.length == pidList.length;
-      @ ensures (\forall int i; 0 <= i && i < pidList.length; pidList[i] != pathId);
-      @ ensures (\forall int i; 0 <= i && i < pList.length;!pList[i].equals(\old(getPathById(pathId))));
-      @ also
-      @ public exceptional_behavior
-      @ assignable \nothing;
-      @ signals (PathIdNotFoundException e) !containsPathId(pathId);
-      @*/
     public void removePathById(int pathId) throws PathIdNotFoundException {
         if (!containsPathId(pathId)) {
             throw new PathIdNotFoundException(pathId);
@@ -172,18 +99,13 @@ public class MyPathContainer implements PathContainer {
         plist.remove(path);
         for (int i : path) {
             int tmp = nodes.get(i);
-            nodes.put(i,tmp-1);
+            nodes.put(i, tmp - 1);
             if (tmp == 1) {
                 nodes.remove(i);
             }
         }
     }
 
-    /*@ ensures \result == (\num_of int[] nlist;
-           (\forall int i,j; 0 <= i && i < pList.length && 0 <= j && j < pList[i].size();
-           (\exists int k; 0 <= k && k < nlist.length; nlist[k] == pList[i].getNode(j)));
-           (\forall int m, n; 0 <= m && m < n && n < nlist.length; nlist[m] != nlist[n]));
-      @*/
     public /*@pure@*/int getDistinctNodeCount() { //在容器全局范围内查找不同的节点数
         return nodes.size();
     }
